252bf8c224604b497fd5787397d403dd1c7c898c
HHllmn <118361459+HHllmn@users.noreply.github.com>
2022-11-29 15:54:35 +0000

    Add files via upload

--- core/src/chef/mygdx/game/MyGdxGame.java
+++ core/src/chef/mygdx/game/MyGdxGame.java
@@ -4,28 +4,52 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
 
+import com.badlogic.gdx.math.Rectangle; //Used to represent chefs and surroundings
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Texture ChefImage; //Image of the chef variable
+	private Texture BGImage; //Image for the background
+	private OrthographicCamera camera; //Camera which will be used to view the game
+	private SpriteBatch batch; //Sprite batch which will use the ChefImage to display the character
+	private Rectangle ChefA; //Creates ChefA Rectangle/Hitbox
+
 	@Override
-	public void create () {
+	public void create() {
+		//Loads the images for Chef and Background (BG) in, 64x64 pixels each
+		//ChefImage = new Texture(Gdx.files.internal("ChefImage.png"));
+		//BGImage = new Texture(Gdx.files.internal("BGImage.png"));
+
+		//Creates the camera and sprite batch
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480); //Camera shows the whole screen (currently set to 800x480 (android landscape))
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		//Create Chef rectangle
+		ChefA = new Rectangle();
+		ChefA.x = 800 / 2 - 64 / 2;
+		ChefA.y = 20;
+		ChefA.width = 100;
+		ChefA.height = 50;
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 1, 0);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(ChefA.x, ChefA.y);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
--- desktop/src/chef/mygdx/game/DesktopLauncher.java
+++ desktop/src/chef/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,10 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Chef Project");
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
